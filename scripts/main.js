dfc3914847c596fa7d27781a8c14c024050f40ad
StormKing969 <wijesinghesaj@gmail.com>
2021-11-26 15:48:31 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,8 @@ const commence_btn = $(".buttons .start_quiz");
 const quiz_box = $(".quiz_box");
 const timeCounter = $(".timer_sec");
 const timer_visual_line = $(".timer_visual_line");
+const quit_quiz_completely_btn = $(".quit_quiz_completely");
+const start_quiz_again_btn = $(".start_quiz_again");
 
 // Start Quiz Clicked
 start_btn.click(e => {
@@ -23,12 +25,23 @@ start_btn.click(e => {
 // Exit Button Clicked
 exit_btn.click(e => {
     rule_box.removeClass("activeRule");
+    // result_box.removeClass("activeResult");
 });
 
 // // Exit Button Clicked
 // $(".buttons").on('click', exit_btn, e => {
 //     rule_box.removeClass("activeRule");
 // });
+
+// End Button Clicked At The End
+quit_quiz_completely_btn.click(e => {
+    window.location.reload();
+});
+
+// Restart Button Clicked At The End
+start_quiz_again_btn.click(e => {
+    window.location.reload();
+});
 
 // Start Button Clicked
 commence_btn.click(e => {
@@ -53,14 +66,20 @@ let question_number = 1;
 let counter;
 let timeValue = 15;
 let timeBar = 0;
+let userScore = 0;
 
 const  next_btn = $(".next_btn");
+const result_box = $(".result_box");
+
+
 
 // Next Button Clicked
 next_btn.click(e => {
     if (question_count < questions.length - 1) {
         question_count++;
         question_number++;
+    } else {
+        showResult();
     }
     showQuestions(question_count);
     questionCounter(question_number);
@@ -72,6 +91,7 @@ next_btn.click(e => {
     timerLine(timeBar);
     
     next_btn.css("display", "none");
+
 });
 
 function showQuestions(index) {
@@ -89,13 +109,6 @@ function showQuestions(index) {
     const option = $(".option").attr("onclick", "optionSelected(this)");
 }
 
-function questionCounter(question_number) {
-    const bottom_counter = $(".total_questions");
-    let totalQuestion_tag = '<span><p>' + question_number + '</p>of<p>'+ questions.length +'</p>Questions</span>';
-    bottom_counter.html(totalQuestion_tag);
-}
-
-
 function optionSelected(answer) {
     clearInterval(counter);
     clearInterval(lineVisual);
@@ -105,7 +118,8 @@ function optionSelected(answer) {
     
     if (userAnswer == correctAnswer) {
         answer.classList.add("correct")
-        // console.log("Correct Answer")
+        userScore += 1;
+        console.log(userScore)
     } else {
         // console.log("Too Bad")
         answer.classList.add("wrong")
@@ -134,6 +148,27 @@ function optionSelected(answer) {
     next_btn.css("display", "block");
 }
 
+function showResult() {
+    rule_box.removeClass("activeRule");
+    quiz_box.removeClass("activeQuiz");
+    result_box.addClass("activeResult");
+
+    const scoreText = $(".score_text");
+    if (userScore < (questions.length / 4)) {
+        let useScore_tag = '<span>Unfortunately, you only got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.html(useScore_tag);
+    } else if (userScore < (questions.length / 2)) {
+        let useScore_tag = '<span>Not bad, you got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.html(useScore_tag);
+    } else if (userScore < ((questions.length / 4) * 3)) {
+        let useScore_tag = '<span>Good Job, you got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.html(useScore_tag);
+    } else {
+        let useScore_tag = '<span>Congratulation, you got <p>' + userScore + '</p> out of <p>' + questions.length + '</p></span>';
+        scoreText.html(useScore_tag);
+    }
+}
+
 function startTimer(time) {
     counter = setInterval(timer, 1000);
 
@@ -159,10 +194,10 @@ function timerLine(timeLength) {
     timer_visual_line.css("background", "#007bff");
 
     function visualLine() {
-        lineFraction = Math.ceil(100 - ((timeLength / timeValue) * 100)) + "%";
+        lineFraction = ((timeLength / timeValue) * 100) + "%";
         lineWidth = timer_visual_line.css("width", lineFraction);
         // console.log(lineFraction)
-        if (lineFraction == "20%") {
+        if (lineFraction == "80%") {
             timer_visual_line.css("background", "#d41a2d");
         }
         timeLength++;
@@ -171,4 +206,10 @@ function timerLine(timeLength) {
             clearInterval(lineVisual);
         }
     }
+}
+
+function questionCounter(question_number) {
+    const bottom_counter = $(".total_questions");
+    let totalQuestion_tag = '<span><p>' + question_number + '</p>of<p>'+ questions.length +'</p>Questions</span>';
+    bottom_counter.html(totalQuestion_tag);
 }
